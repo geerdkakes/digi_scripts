8a9101a42a3bdcf0a8432ff1d80d078d878d56c6
Geerd Kakes <geerd@kakes.net>
2022-11-14 21:07:32 +0100

    change script

--- log_signal_strength.sh
+++ log_signal_strength.sh
@@ -4,6 +4,6 @@ echo "timestamp,rsrp,rssi"
 PASSWORD="thisissecret"
 
 while true; do
-sshpass -p ${PASSWORD} ssh admin@192.168.1.1 "cli -c \"show modem name wwan1\"" | ./digi_modem.py
+sshpass -p ${PASSWORD} ssh admin@192.168.1.1 "cli -c \"show modem name wwan1\"" | ./filter_signal_strength.py
 sleep 0.2
 done
